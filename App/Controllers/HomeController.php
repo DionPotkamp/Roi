419e4ae1259e65043f802c411e2d618d54820b0a
Dion Potkamp <dion.potkamp@gmail.com>
2020-07-05 23:40:53 +0200

    add Home Controller

---
+++ App/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController
+{
+    public function index(Request $request)
+    {
+        return new Response('Home');
+    }
+
+    public function hello(Request $request, $name)
+    {
+        return new Response('Hello '.$name);
+    }
+}
