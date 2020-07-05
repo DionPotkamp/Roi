316e40a5cf9f78303ed2f1979a978da7903ea5d0
Dion Potkamp <dion.potkamp@gmail.com>
2020-07-05 23:40:46 +0200

    add LeapYear Controller

---
+++ App/Controllers/Calendar/LeapYearController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controllers\Calendar;
+
+use App\Models\Calendar\LeapYear;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LeapYearController
+{
+    public function index(Request $request, $year)
+    {
+        $leapYear = new LeapYear();
+        if ($leapYear->isLeapYear($year)) {
+            return new Response('Yep, this is a leap year!');
+        }
+
+        return new Response('Nope, this is not a leap year.');
+    }
+}
