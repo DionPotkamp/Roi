25ec2fdf198f02a9d6666f5ab3af291320ec356b
Dion Potkamp <dion.potkamp@gmail.com>
2020-07-05 23:41:03 +0200

    add Helper functions

---
+++ App/Helpers/helpers.php
@@ -0,0 +1,31 @@
+<?php
+
+if (!function_exists('dd')) {
+    function dd($dumpMe, $_ = null){
+        $args = func_get_args();
+        echo '<code><pre class="d-block p-3 bg-dark text-light">';
+        foreach ($args as $arg) {
+            var_dump($arg);
+            echo '<hr>';
+        }
+        echo '</pre></code>';
+        die();
+    }
+}
+
+if (!function_exists('env')) {
+    /**
+     * @param $key
+     * @param null $default
+     * @return mixed|null
+     */
+    function env($key, $default = null){
+        $configKey = explode('.', $key);
+        $config = include __DIR__.'/../Config/'.$configKey[0].'.php';
+        if (array_key_exists($configKey[1], $config)) {
+            return $config[$configKey[1]];
+        } else {
+            return $default;
+        }
+    }
+}
