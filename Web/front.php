86801f24fddc69552d4fb119069bdf6d7d6588b4
Dion <dion.potkamp@gmail.com>
2020-08-06 00:00:21 +0200

    implement events

--- Web/front.php
+++ Web/front.php
@@ -1,14 +1,18 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel;
 use Symfony\Component\Routing;
 use Roi\Route;
 
 // Create the request
 $request = Request::createFromGlobals();
+$requestStack = new RequestStack();
 
 // Define the routes
 $route = new Route();
@@ -23,8 +27,14 @@ $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 $controllerResolver = new ControllerResolver();
 $argumentResolver = new ArgumentResolver();
 
+$dispatcher = new EventDispatcher();
+$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
+$dispatcher->addSubscriber(new HttpKernel\EventListener\ErrorListener('Roi\ErrorHandler::exception'));
+$dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
+$dispatcher->addSubscriber(new Roi\Events\StringResponseListener());
+
 // Initialize new Roi Framework and get the response
-$framework = new Roi\Framework($matcher, $controllerResolver, $argumentResolver);
+$framework = new Roi\Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
 $response = $framework->handle($request);
 
 // Send the response(content) of the request back to the user
