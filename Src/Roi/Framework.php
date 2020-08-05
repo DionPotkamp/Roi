8f95e86ed475f5624d31d0513f979d2a470ac82e
Dion <dion.potkamp@gmail.com>
2020-08-05 23:59:17 +0200

    extend HttpKernel

--- Src/Roi/Framework.php
+++ Src/Roi/Framework.php
@@ -3,49 +3,9 @@
 
 namespace Roi;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
-use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
-use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\HttpKernel\HttpKernel;
 
-class Framework
+class Framework extends HttpKernel
 {
-    protected $matcher;
-    protected $controllerResolver;
-    protected $argumentResolver;
-
-    public function __construct(UrlMatcherInterface $matcher, ControllerResolverInterface $controllerResolver, ArgumentResolverInterface $argumentResolver)
-    {
-        $this->matcher = $matcher;
-        $this->controllerResolver = $controllerResolver;
-        $this->argumentResolver = $argumentResolver;
-    }
-
-    public function handle(Request $request)
-    {
-        $this->matcher->getContext()->fromRequest($request);
-
-        try {
-            // Searches through the defined routes with the given path/url
-            // Returns the parameters from the route and adds it to the request
-            $request->attributes->add($this->matcher->match($request->getPathInfo()));
-
-            // Get the controller and the arguments from the request
-            $controller = $this->controllerResolver->getController($request);
-            $arguments = $this->argumentResolver->getArguments($request, $controller);
-
-            // Call the controller and pass it the arguments
-            return call_user_func_array($controller, $arguments);
-
-        } catch (ResourceNotFoundException $exception) {
-            // Throw custom not found error
-            return new Response('Not Found', 404);
-        } catch (\Exception $exception) {
-            // Throw custom server error error
-            return new Response('An error occurred<br><pre>'.$exception, 500);
-        }
-    }
 
 }
