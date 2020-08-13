737f1996e7730b202d3a94dceac3eef7a618551d
Dion <dion.potkamp@gmail.com>
2020-08-13 22:51:17 +0200

    add twig config

--- App/Config/templates.php
+++ App/Config/templates.php
@@ -1,5 +1,10 @@
 <?php
 return [
+    'extension' => '.twig',
     'dir' => __DIR__.'/../Templates',
-    'cache' => __DIR__.'/../../var/cache/templates'
+    'cache' => __DIR__.'/../../var/cache/templates',
+
+    'twig_extensions' => [
+        'Function' => 'Src\\Twig\\Extensions',
+    ]
 ];
