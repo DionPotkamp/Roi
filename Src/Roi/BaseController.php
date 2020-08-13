04c54cb7794b1e0ac0fb3189bd1d6b373bf72189
Dion <dion.potkamp@gmail.com>
2020-08-13 22:52:35 +0200

    add twig environment to basecontroller

--- Src/Roi/BaseController.php
+++ Src/Roi/BaseController.php
@@ -4,18 +4,55 @@
 namespace Roi;
 
 use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 class BaseController
 {
-    private $twigEnv;
+    protected $twigEnv;
 
-    public function __construct(Environment $twigEnv)
+    public function __construct()
     {
-        $this->twigEnv = $twigEnv;
+        $this->twigEnv = new Environment(new FilesystemLoader(env('templates.dir')), [
+            'cache' => env('templates.cache'), // Cache directory
+            'debug' => env('app.debug')
+        ]);
+        $this->addTwigFunctions();
     }
 
-    public function render($array) {
-        return $this->twigEnv->render($array);
+    public function render($name, $context = array())
+    {
+        return $this->twigEnv->render($name.env('templates.extension', '.twig'), $context);
+    }
+
+    /**
+     * WIP
+     */
+    public function addTwigFunctions()
+    {
+        $registerThese = env('templates.twig_extensions');
+
+        // Addable extensions: TokenParser, NodeVisitor, Function, Filter, Test, Operator
+        $extensions = [
+            'TokenParser', 'NodeVisitor', 'Function', 'Filter', 'Test', 'Operator'
+        ];
+
+        foreach ($registerThese as $type => $namespaceClass) {
+            if (!in_array($type, $extensions)) {
+                throw new \Error("The type(${type}) for the twig extensions does not exist");
+            }
+
+            $twigGetExtensions = 'get' . $type . 's';
+            $twigAddExtension = 'add' . $type;
+
+            // Create a new instance
+            $instance = new $namespaceClass();
+            $twigExtensions = $instance->$twigGetExtensions();
+
+            // Add the extension to the Twig environment
+            foreach ($twigExtensions as $twigExtension) {
+                $this->twigEnv->$twigAddExtension($twigExtension);
+            }
+        }
     }
 
 }
