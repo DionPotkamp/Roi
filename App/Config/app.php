092b699db9ee514f6b86506ee02ce0b5101ea117
Dion Potkamp <dion.potkamp@gmail.com>
2020-08-14 15:24:40 +0200

    add SEO

--- App/Config/app.php
+++ App/Config/app.php
@@ -1,7 +1,17 @@
 <?php
 return [
-    'name' => 'Testing',
+    'name' => 'Roi',
+    'description' => 'A Basic Framework, It Helps Me Understand MVC\'s',
+    'keywords' => 'roi,framework,mvc',
     'author' => 'Dion Potkamp',
-    'locale' => 'nl',
+    'image' => '',
+
+    'url' => '127.0.0.1:8000',
+    'robots' => 'index, follow',
+    'revisitAfter' => '7 days',
+
+    'locale' => 'en',
+    'language' => 'english',
+
     'debug' => true
 ];
